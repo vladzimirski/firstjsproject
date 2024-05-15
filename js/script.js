cfe3eb0eb7f57e028f9a227a8437da076d317c86
Pavel <vladzimirskiy9@gmail.com>
2024-05-15 18:34:16 +0900

    zakonchil urok

--- js/script.js
+++ js/script.js
@@ -18,3 +18,55 @@ personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
 
 
+if (6 == 9) {
+	console.log("ok");
+} else {
+	console.log("error");
+}
+
+// if (num < 49) {
+// 	console.log("Error")
+// }else if (num > 100) {
+// 	console.log("mnogo");
+// }else {
+// 	console.log("Yes")
+// }
+
+// (num === 50) ? console.log("ok") : console.log("error");
+const num = 50;
+
+switch (num) {
+	case 49:
+		console.log("error");
+		break;
+	case 100:
+		console.log("Mnogo");
+		break;
+	case 51:
+		console.log("Ok");
+		break;
+	default:
+		console.log("error");
+		break;
+}
+
+// const humburger = 2,
+// 	fries = 1;
+// if (humburger === 3 && fries) { 
+// 	console.log(`Все сыты`);
+// } else {
+// 	console.log(`Мы уходим`);
+// }
+
+const humburger = 3,
+	fries = 3,
+	cola = 0,
+	nagets = 2;
+if (humburger === 3 && cola === 2 || fries === 3 && nagets) {
+	console.log(`Все довольны`);
+} else {
+	console.log(`Мы уходим`);
+}
+
+
+
